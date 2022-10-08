0fa51f73317899dc605cb5a49e146670308bab2f
yashweblife <yashweblife20@gmail.com>
2022-10-08 05:55:12 -0500

    Added camera functions

--- src/lib/Camera.ts
+++ src/lib/Camera.ts
@@ -1,11 +1,31 @@
+
+
 export class Camera{
-    constructor(){}
-    public init = ()=>{}
-    public play = ()=>{}
-    public stop = ()=>{}
-    public close = ()=>{}
-    public pause = ()=>{}
-    public capture = ()=>{}
+    public stream:MediaStream|null;
+    public video:HTMLVideoElement
+    constructor(video:HTMLVideoElement = document.createElement("video")){
+        this.video = video;
+    }
+    public init = async(constraints?:MediaStreamConstraints)=>{
+        const stream = await navigator.mediaDevices.getUserMedia(constraints)
+        this.stream = stream;
+        this.video.srcObject = this.stream;
+        this.video.play()
+    }
+    public play = ()=>{
+        this.video.play()
+    }
+    public pause = ()=>{
+        this.video.pause()
+    }
+    public close = ()=>{
+        this.stream?.getTracks().forEach((track:MediaStreamTrack)=>{
+            this.stream?.removeTrack(track)
+        })
+    }
+    public capture = ()=>{
+        
+    }
     public reverse = ()=>{}
 
 }
