df8b18f4ec6e829e3fa795bcfeea55ab06fec206
yashweblife <yashweblife20@gmail.com>
2022-10-08 04:31:52 -0500

    Populated keyboard functions

--- src/lib/Keyboard.ts
+++ src/lib/Keyboard.ts
@@ -1,1 +1,10 @@
-export class Keyboard{}
+export class Keyboard{
+    lastKey:string;
+
+    constructor(){}
+
+    public handleClick = (e:Event)=>{}
+    public handleDown = (e:Event)=>{}
+    public handleUp = (e:Event)=>{}
+
+}
