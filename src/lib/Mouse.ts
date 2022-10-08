6366ced5827b68c8c88ba463778077c3cb762791
yashweblife <yashweblife20@gmail.com>
2022-10-08 04:32:02 -0500

    Populated mouse function

--- src/lib/Mouse.ts
+++ src/lib/Mouse.ts
@@ -1,1 +1,13 @@
-export class Mouse{}
+import { Vector } from "./Vector";
+
+export class Mouse{
+    pos:Vector;
+    click:Boolean = false;
+    offset:Vector;
+    constructor(){}
+    public handleMove = (e:Event)=>{}
+    public handleClick = (e:Event)=>{}
+    public handleDown = (e:Event)=>{}
+    public handleUp = (e:Event)=>{}
+
+}
