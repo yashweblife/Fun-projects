e0a2b42d76f16921248e057ad85c3d1904b290c1
yashweblife <yashweblife20@gmail.com>
2022-10-08 05:55:34 -0500

    Completed some more functions

--- src/lib/Canvas.ts
+++ src/lib/Canvas.ts
@@ -1,12 +1,11 @@
+import {Vector} from "./Vector"
+interface CircleInterface{pos:Vector; radius:number; fillColor?:string;strokeColor?:string; stroke?:Boolean; fill?:Boolean}
+interface RectInterface{pos:Vector; size:Vector; fillColor?:string;strokeColor?:string; stroke?:Boolean; fill?:Boolean, angle?:number}
 /**
  * @class
  * # Canvas
  * ### This is a library to simplify the use of HTMl canvas element
  */
-
-import {Vector} from "./Vector"
-
-interface CircleInterface{pos:Vector; radius:number; fillColor?:string;strokeColor?:string; stroke?:Boolean; fill?:Boolean}
 export class Canvas{
     public dom:HTMLCanvasElement;
     public ctx:CanvasRenderingContext2D;
@@ -15,7 +14,6 @@ export class Canvas{
     constructor(canvas:HTMLCanvasElement = document.createElement("canvas")){
         this.dom = canvas;
         this.ctx = this.dom.getContext('2d')!;
-
     }
 
     public circle = ({pos=new Vector(0,0), radius=5, fillColor="red",strokeColor="black", stroke=false, fill=true}:CircleInterface)=>{
@@ -27,8 +25,16 @@ export class Canvas{
         if(fill){this.ctx.fill()}
         if(stroke){this.ctx.stroke()}
     }
-    public rect = ()=>{}
-    public reverseRect = ()=>{}
+    public rect = ({pos=new Vector(0,0), size=new Vector(100,100), fillColor="red", strokeColor="black", stroke=false, fill=true, angle=0}:RectInterface)=>{
+        this.ctx.beginPath()
+        this.ctx.fillStyle = fillColor;
+        this.ctx.strokeStyle = strokeColor;
+        this.ctx.rect(pos.x, pos.y, size.x, size.y);
+        this.ctx.closePath()
+        if(fill===true) this.ctx.fill();
+        if(stroke===true) this.ctx.stroke();
+    }
+    public reverseRect = ({pos, size, fillColor, strokeColor, stroke, fill, angle})=>{}
     public drawPath = ()=>{}
     public clear = ()=>{}
     
