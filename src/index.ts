82b9c9fa09fc9805ac0ba1088e29b289acaa6537
yashweblife <yashweblife20@gmail.com>
2022-10-09 07:39:45 -0500

    imported style

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import { CollisionDetectionDemo } from "./projects/CollisionDetection"
-import {GravityDemo} from "./projects/Gravity"
-import {CompressorDemo} from "./projects/Compression"
-import {BinaryTree} from "./projects/DataStructures"
-
-const t= new BinaryTree(5)
-for(var i=0;i<10;i++){
-    t.addData(Math.floor(Math.random()*100));
-}
-t.traverse()
+import "./styles/index.scss"
