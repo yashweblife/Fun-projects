02d05eaddfa4fd9798aa6f188231028a5a6a70d3
yashweblife <yashweblife20@gmail.com>
2022-10-09 21:41:17 -0500

    testing sort

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 import "./styles/index.scss"
+import {QuickSort} from "./projects/Sorting"
+
+const arr:number[] = []
+for(var i =0;i<10;i++){
+    //arr.push(Math.floor(Math.random()*100))
+    arr.push(10-i)
+}
+const b = new QuickSort(arr)
+b.update()
