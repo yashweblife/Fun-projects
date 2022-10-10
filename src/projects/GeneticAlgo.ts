2dd8db37b76c6f6eb235bb6c30c16756d397e398
yashweblife <yashweblife20@gmail.com>
2022-10-09 21:40:53 -0500

    Created code structure

---
+++ src/projects/GeneticAlgo.ts
@@ -0,0 +1,11 @@
+import {Ball} from '../lib/Ball'
+class GeneticAlgoDemo{
+    public agents:Ball[];
+    private findAverage = ()=>{}
+    private selection = ()=>{}
+    private breeding = ()=>{}
+    private update = ()=>{
+        requestAnimationFrame(this.update)
+    }
+    public app = ()=>{}
+}
