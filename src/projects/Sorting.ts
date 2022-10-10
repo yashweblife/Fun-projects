fa2e7af8b8391e5a1f649c290b2ede4a1d69fbe7
yashweblife <yashweblife20@gmail.com>
2022-10-09 21:31:17 -0500

    Created Quick sort

--- src/projects/Sorting.ts
+++ src/projects/Sorting.ts
@@ -21,4 +21,42 @@ export class BubbleSort{
 
 }
 
-class QuickSort{}
+export class QuickSort{
+    public data:number[];
+
+    constructor(data:number[]){
+        this.data = data;
+    }
+    public update = ()=>{
+        const splitter = (arr:number[],itter:number=0):number[]=>{
+            if(itter == arr.length-1) {
+                console.log("test", arr);
+                return(arr);
+            }
+            itter+=1;
+            const index = 0;
+            const pivot = arr[index]
+            const more:number[] = []
+            const less:number[] = []
+            for(var i=0;i<arr.length;i++){
+                if(i!=index){
+                    if(arr[i]<=pivot){
+                        less.push(arr[i])
+                    }else{
+                        more.push(arr[i])
+                    }
+                }
+            }
+            const output = less;
+            output.push(pivot)
+            more.forEach((m:number)=>{
+                output.push(m)
+            })
+            console.log(pivot, output)
+            splitter(output,itter)
+        }
+        const op = splitter(this.data);
+        console.log("Final : ", op)
+    }
+
+}
