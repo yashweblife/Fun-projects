c8d565ae04f66441ba2525d0f10bec94b9662cda
yashweblife <yashweblife20@gmail.com>
2022-10-09 20:48:52 -0500

    Completed bubble sort

--- src/projects/Sorting.ts
+++ src/projects/Sorting.ts
@@ -1,5 +1,23 @@
-class BubbleSort{
-    public data:any;
+export class BubbleSort{
+    public data:number[];
+    constructor(data:number[]){
+        this.data = data;
+    }
+    public swap = (i1:number, i2:number) =>{
+        const temp = this.data[i1]
+        this.data[i1] = this.data[i2]
+        this.data[i2] = temp
+    }
+    public update = ()=>{
+        for(var i=0;i<this.data.length-1;i++){
+            for(var j=0;j<this.data.length-i-1;j++){
+                if(this.data[j]>this.data[j+1]){
+                    this.swap(j,j+1);
+                }
+            }
+        }
+        console.log(this.data)
+    }
 
 }
 
