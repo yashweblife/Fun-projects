6a96f962b8740ad8e0130358d4ded54eb7cc1028
yashweblife <yashweblife20@gmail.com>
2022-10-16 07:53:32 -0500

    More Features

--- src/projects/PixelArt.ts
+++ src/projects/PixelArt.ts
@@ -1,2 +1,12 @@
-class Grid{}
-class Pixel{}
+import {Vector} from "../lib/Vector"
+class Grid{
+    private size:Vector;
+    private shape:Vector;
+    constructor(){}
+}
+class Pixel{
+    private pos:Vector;
+    private size:Vector;
+    private color:string;
+    constructor(){}
+}
