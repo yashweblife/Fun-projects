908713115778e4d7f539e7a0d13601d217cf475f
yashweblife <yashweblife20@gmail.com>
2022-10-17 09:07:41 -0500

    Simulated geoCentric gravity

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import "./styles/index.scss"
-import {QuickSort} from "./projects/Sorting"
+import {GravityDemo} from "./projects/Gravity"
 
-const arr:number[] = []
-for(var i =0;i<10;i++){
-    //arr.push(Math.floor(Math.random()*100))
-    arr.push(10-i)
-}
-const b = new QuickSort(arr)
-b.update()
+const simul = new GravityDemo(document.body)
+
+simul.app()
