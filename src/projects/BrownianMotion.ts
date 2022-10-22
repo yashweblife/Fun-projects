10bf8939c57036190703b58c64ed7950b7976bc3
yashweblife <yashweblife20@gmail.com>
2022-10-22 18:18:35 -0500

    Comments

--- src/projects/BrownianMotion.ts
+++ src/projects/BrownianMotion.ts
@@ -1,10 +1,22 @@
 import {Ball} from '../lib/Ball'
 import { Canvas } from '../lib/Canvas'
 import { Vector } from '../lib/Vector'
+/**
+ * ## Brownian Motion 
+ * This is a phenonemon that can be observerd on the microscopic scale.
+ * It is where larger particels seem to follow a random motion when suspended in a fluid.
+ * This happens when tinier particles with higher kinetic energy collide with larger particles.
+ * The effect can be simulated by applying random forces to the Ball object.
+ * @functions app()
+ */
 export class BrownianMotionDemo{
     public balls:Ball[] = []
     public canvas:Canvas
     private time:number = 0
+    /**
+     * 
+     * @param parent A parent for the canvas object
+     */
     constructor(parent:HTMLElement){
         this.canvas = new Canvas()
         parent.append(this.canvas.dom)
@@ -23,6 +35,9 @@ export class BrownianMotionDemo{
         })
         requestAnimationFrame(this.animate)
     }
+    /**
+     * Call the app function to get the animation running
+     */
     public app = ()=>{
         this.animate()
     }
