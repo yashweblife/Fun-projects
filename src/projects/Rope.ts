b668f6348ad9c52b84dda869406f853295d7ed7e
yashweblife <yashweblife20@gmail.com>
2022-10-23 17:39:24 -0500

    Completed framework

--- src/projects/Rope.ts
+++ src/projects/Rope.ts
@@ -1,35 +1,54 @@
 import {Ball}from "../lib/Ball"
 import { Canvas } from "../lib/Canvas";
 import { Vector } from "../lib/Vector";
+import {Mouse} from "../lib/Mouse";
+
+var mouse = new Mouse();
+window.addEventListener('mousemove',mouse.move)
 class Rope{
     public links:Ball[] = []
-    public linkSize:number = 100;
+    public linkSize:number = 1;
+    private wind:Vector = new Vector(0.2,0);
+    private gravity:Vector = new Vector(0,0.8);
     constructor(canvas:Canvas){
-        for(var i=0;i<5;i++){
-            //this.links.push(new Ball(new Vector(canvas.width/2,canvas.height/2)));
-            this.links.push(Ball.random(0, canvas.width));
+        for(var i=0;i<50;i++){
+            this.links.push(new Ball(new Vector(canvas.width/2,canvas.height/2)));
+            this.links[i].size= 5
         }
-        this.links[0].color = "red"
-        this.links[0].size= 10
     }
     public update = (canvas:Canvas)=>{
-        // this.links[0].pos.x = canvas.width/2
-        // this.links[0].pos.y = canvas.height/2
+        //  this.links[0].pos.x = canvas.width/2
+        //  this.links[0].pos.y = canvas.height/2
         for(var i =0;i<this.links.length;i++){
-            for(var j=0;j<this.links.length;j++){
-                if(i!=j){
-                    var nVec = Vector.VecFromSub(this.links[i].pos, this.links[j].pos)
-                    nVec.normalize();
-                    nVec.scalar(1)
-                    this.links[i].acc = nVec
+            if(i!=this.links.length-1){
+                if(this.links[i].pos.dist(this.links[i+1].pos)>this.linkSize+10){
+                    this.links[i+1].attract(this.links[i],20)
+                }else if(this.links[i].pos.dist(this.links[i+1].pos)<this.linkSize-10){
+                    this.links[i+1].repel(this.links[i],5)
+                }else{
+                    this.links[i+1].acc.scalar(0)
+                    this.links[i+1].vel.scalar(0)
                 }
             }
+            this.links[i].addForce(this.gravity)
+            //this.links[i].addForce(this.wind)
+            
         }
+        this.attract(mouse)
+
+        for(var i =0;i<this.links.length-1;i++){
+            canvas.line(this.links[i].pos, this.links[i+1].pos)
+        }
+        
         this.links.forEach((ball:Ball)=>{
             ball.update()
             ball.bound(canvas,{x:false,y:false  })
-            ball.draw(canvas)
+            //ball.draw(canvas)
         })
+    }
+    public attract = (mouse:Mouse)=>{
+        const b = this.links[0]
+        b.attract(mouse,2)
     }
 }
 export class RopeDemo{
