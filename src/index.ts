6dfad55e37ae7f4a90246aeec2aa8862791586b7
yashweblife <yashweblife20@gmail.com>
2022-10-24 12:20:56 -0500

    Testing stuff

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import "./styles/index.scss";
-import { RopeDemo } from "./projects/Rope";
-import { CollisionDetectionDemo } from "./projects/CollisionDetection";
-import { BrownianMotionDemo } from "./projects/BrownianMotion";
-import { CompressorDemo } from "./projects/Compression";
-import { GravityDemo } from "./projects/Gravity";
-import { SortingDemo } from "./projects/Sorting";
-import { DataStructureDemo } from "./projects/DataStructures";
-import { ImageClassifierDemo } from "./projects/ImageClassifier";
-import { LinearRegressionDemo } from "./projects/LinearRegression";
-import { RayCastingDemo } from "./projects/RayCasting";
-import { GeneticAlgoDemo } from "./projects/GeneticAlgo";
+// import { RopeDemo } from "./projects/Rope";
+// import { CollisionDetectionDemo } from "./projects/CollisionDetection";
+// import { BrownianMotionDemo } from "./projects/BrownianMotion";
+// import { CompressorDemo } from "./projects/Compression";
+// import { GravityDemo } from "./projects/Gravity";
+// import { SortingDemo } from "./projects/Sorting";
+// import { DataStructureDemo } from "./projects/DataStructures";
+// import { ImageClassifierDemo } from "./projects/ImageClassifier";
+// import { LinearRegressionDemo } from "./projects/LinearRegression";
+// import { RayCastingDemo } from "./projects/RayCasting";
+// import { GeneticAlgoDemo } from "./projects/GeneticAlgo";
+
+import {BrownianMotionDemo} from './projects'
+
+const b = new BrownianMotionDemo(document.body)
+b.app()
