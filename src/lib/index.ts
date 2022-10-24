c1137ed5f61e721615e5b295a9f385b796caf6a5
yashweblife <yashweblife20@gmail.com>
2022-10-24 12:20:46 -0500

    Added central export system

---
+++ src/lib/index.ts
@@ -0,0 +1,19 @@
+import { Vector } from "./Vector";
+import { PhysicsObject } from "./PhysicsObject";
+import { Ball } from "./Ball";
+import { Mouse } from "./Mouse";
+import { Keyboard } from "./Keyboard";
+import { Geometry } from "./Geometry";
+import { Graph } from "./Graph";
+import { Canvas } from "./Canvas";
+
+export {
+  Vector,
+  Ball,
+  Mouse,
+  PhysicsObject,
+  Keyboard,
+  Geometry,
+  Graph,
+  Canvas,
+};
