060910ea67dd8e95f4c02413d87883907ec3ebd4
yashweblife <yashweblife20@gmail.com>
2022-10-24 12:21:09 -0500

    Added central export system

---
+++ src/projects/index.ts
@@ -0,0 +1,28 @@
+import { BrownianMotionDemo } from "./BrownianMotion";
+import { CollisionDetectionDemo } from "./CollisionDetection";
+import { CompressorDemo } from "./Compression";
+import { DataStructureDemo } from "./DataStructures";
+import { GeneticAlgoDemo } from "./GeneticAlgo";
+import { GravityDemo } from "./Gravity";
+import { ImageClassifierDemo } from "./ImageClassifier";
+import { LinearRegressionDemo } from "./LinearRegression";
+import { PixelArtDemo } from "./PixelArt";
+import { RayCastingDemo } from "./RayCasting";
+import { RopeDemo } from "./Rope";
+import { SortingDemo } from "./Sorting";
+
+
+export {
+    BrownianMotionDemo,
+    CollisionDetectionDemo,
+    CompressorDemo,
+    DataStructureDemo,
+    GeneticAlgoDemo,
+    GravityDemo,
+    ImageClassifierDemo,
+    LinearRegressionDemo,
+    PixelArtDemo,
+    RayCastingDemo,
+    RopeDemo,
+    SortingDemo
+}
