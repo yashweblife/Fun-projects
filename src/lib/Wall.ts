871c02ee4f1a3db967c445eff9756fef049faf1b
yashweblife <yashweblife20@gmail.com>
2022-10-26 23:24:49 -0500

    Meh

--- src/lib/Wall.ts
+++ src/lib/Wall.ts
@@ -2,13 +2,13 @@ import {Vector} from './Vector'
 import {PhysicsObject} from './PhysicsObject'
 import { Canvas } from './Canvas'
 export class Wall extends PhysicsObject{
-    size:Vector;
-    angle:number;
+    private angle:number;
+    
     public draw = (c:Canvas)=>{
-        c.rect({
-            pos:this.pos,
-            size:this.size,
-            angle:this.angle
-        })
+        // c.rect({
+        //     pos:this.pos,
+        //     size:this.size,
+        //     angle:this.angle
+        // })
     }
 }
