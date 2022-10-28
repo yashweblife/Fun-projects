64f4c68acc8ef26066c289b24aae61fc5c87e0d7
yashweblife <yashweblife20@gmail.com>
2022-10-27 19:58:20 -0500

    Made stuff

--- src/lib/Geometry.ts
+++ src/lib/Geometry.ts
@@ -1,15 +1,37 @@
 import {Vector} from './Vector'
 import { PhysicsObject } from './PhysicsObject'
-class Vertex {
-    private pos:Vector;
+import { Canvas } from './Canvas';
+class Vertex extends Vector {
+    public pos:Vector;
+    constructor(vec:Vector){
+        super();
+        this.pos = vec;
+    }
 }
 class Edge {
-    private start:Vertex;
-    private end:Vertex
+    private start:Vector;
+    private end:Vector;
+    constructor(vec1:Vector, vec2:Vector){
+        this.start = new Vertex(vec1);
+        this.end = new Vertex(vec2);
+    }
+    public draw = (c:Canvas)=>{
+        c.line(this.start, this.end)
+    }
 }
-class Shape {
+
+export class Geometry {
     private edges:Edge[]
-}
-export class Geometry extends PhysicsObject {
-    shape:Shape[]
+    public vertices:Vector[]
+    constructor(vec:Vector[]){
+        this.edges = []
+        this.vertices = vec
+        for(var i=0;i<vec.length-1;i++){
+            this.edges.push(new Edge(vec[i],vec[i+1]))
+        }
+        this.edges.push(new Edge(vec[vec.length-1],vec[0]))
+    }
+    public draw = (c:Canvas)=>{
+        c.drawPath(this.vertices)
+    }
 }
