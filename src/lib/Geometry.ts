2c086b10a424e8d5718019ed06f252ed677c8f47
yashweblife <yashweblife20@gmail.com>
2022-10-28 14:03:30 -0500

    Stuff works now

--- src/lib/Geometry.ts
+++ src/lib/Geometry.ts
@@ -19,19 +19,63 @@ class Edge {
         c.line(this.start, this.end)
     }
 }
-
+interface BoundBox{
+    top:Vector;
+    right:Vector;
+    bottom:Vector;
+    left:Vector;
+}
 export class Geometry {
     private edges:Edge[]
     public vertices:Vector[]
+    public bound:BoundBox;
+    public center:Vector;
     constructor(vec:Vector[]){
         this.edges = []
         this.vertices = vec
         for(var i=0;i<vec.length-1;i++){
             this.edges.push(new Edge(vec[i],vec[i+1]))
         }
+        var sum = new Vector()
+        vec.forEach((v:Vector)=>{
+            sum.add(v)
+        })
+        sum.scalar(1/vec.length);
+        this.center = sum;
         this.edges.push(new Edge(vec[vec.length-1],vec[0]))
+        this.findBound()
+    }
+    public findBound = ()=>{
+        var top = new Vector();
+        var left = new Vector();
+        this.vertices.forEach((v:Vector)=>{
+            if(v.y<top.y){
+                top = v
+            }
+            if(v.x<left.x){
+                left = v
+            }
+        })
+        var bottom = top.clone();
+        var right = left.clone();
+        this.vertices.forEach((v:Vector)=>{
+            if(v.y>bottom.y){
+                bottom = v
+            }
+            if(v.x>right.x){
+                right = v
+            }
+        })
+        this.bound = {
+            top:top,
+            left:left,
+            bottom:bottom,
+            right:right
+        }
+        console.table(this.bound)
     }
     public draw = (c:Canvas)=>{
-        c.drawPath(this.vertices)
+        c.fillPath(this.vertices)
+        c.circle({pos:this.center,radius:10,fillColor:"green"})
     }
 }
