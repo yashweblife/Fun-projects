373088a9ba7d18d100c4e8779453b9dcdcb1e623
yashweblife <yashweblife20@gmail.com>
2022-10-31 05:53:11 -0500

    Added cloth

--- src/projects/Rope.ts
+++ src/projects/Rope.ts
@@ -5,12 +5,10 @@ import { Mouse } from "../lib/Mouse";
 
 var mouse = new Mouse();
 window.addEventListener("mousemove", mouse.move);
-class Rope {
+export class Rope {
   public links: Ball[] = [];
   public linkSize: number = 1;
-  private wind: Vector = new Vector(0.2, 0);
-  private gravity: Vector = new Vector(0, 0.8);
-  constructor(canvas: Canvas) {
+  constructor(canvas: Canvas, size:number=10) {
     for (var i = 0; i < 50; i++) {
       this.links.push(
         new Ball(new Vector(canvas.width / 2, canvas.height / 2))
@@ -19,8 +17,7 @@ class Rope {
     }
   }
   public update = (canvas: Canvas) => {
-    //  this.links[0].pos.x = canvas.width/2
-    //  this.links[0].pos.y = canvas.height/2
+
     for (var i = 0; i < this.links.length; i++) {
       if (i != this.links.length - 1) {
         if (
@@ -38,10 +35,7 @@ class Rope {
           this.links[i + 1].vel.scalar(0);
         }
       }
-      this.links[i].addForce(this.gravity);
-      //this.links[i].addForce(this.wind)
     }
-    this.attract(mouse);
 
     for (var i = 0; i < this.links.length - 1; i++) {
       canvas.line(this.links[i].pos, this.links[i + 1].pos);
@@ -58,6 +52,14 @@ class Rope {
     b.attract(mouse, 2);
   };
 }
+
+export class Cloth{
+  private links:Vector[]
+  constructor(){
+    
+  }
+}
+
 export class RopeDemo {
   public canvas: Canvas = new Canvas();
   public rope: Rope = new Rope(this.canvas);
