1d374f74512fba84c011971a1670afb9c1110723
yashweblife <yashweblife20@gmail.com>
2022-11-06 01:39:06 -0500

    Removed semi colons

--- src/projects/CollisionDetection.ts
+++ src/projects/CollisionDetection.ts
@@ -1,46 +1,46 @@
-import { Ball } from "../lib/Ball";
-import { Canvas } from "../lib/Canvas";
+import { Ball } from "../lib/Ball"
+import { Canvas } from "../lib/Canvas"
 
 /**
  * ## Ball Group
  * A simpler/concentrated way to manage the balls in this simulation.
  */
 class BallGroup {
-  private balls: Ball[];
+  private balls: Ball[]
   constructor(canvas: Canvas, size: number = 10) {
-    this.balls = Ball.generate(size, 0, canvas.width);
+    this.balls = Ball.generate(size, 0, canvas.width)
   }
   public handleCollision = () => {
     for (var i = 0; i < this.balls.length; i++) {
       for (var j = 0; j < this.balls.length; j++) {
         if (i != j) {
-          const b1 = this.balls[i];
-          const b2 = this.balls[j];
+          const b1 = this.balls[i]
+          const b2 = this.balls[j]
           if (b1.dist(b2) <= b1.size + b2.size) {
-            b1.repel(b2, 2);
-            b1.vel.scalar(0.8);
+            b1.repel(b2, 2)
+            b1.vel.scalar(0.8)
           } else {
-            b1.attract(b2, 0.001);
+            b1.attract(b2, 0.001)
           }
         }
       }
     }
-  };
+  }
   public update = () => {
     this.balls.forEach((b: Ball) => {
-      b.update();
-    });
-  };
+      b.update()
+    })
+  }
   public bound = (canvas: Canvas) => {
     this.balls.forEach((b: Ball) => {
-      b.bound(canvas, { x: false, y: false });
-    });
-  };
+      b.bound(canvas, { x: false, y: false })
+    })
+  }
   public draw = (canvas: Canvas) => {
     this.balls.forEach((b: Ball) => {
-      b.draw(canvas);
-    });
-  };
+      b.draw(canvas)
+    })
+  }
 }
 /**
  * ## Collision Detection
@@ -51,22 +51,22 @@ class BallGroup {
  *     - Find the direction vector of the ball relative to the other one, reverse it and multiply a scalar to it.
  */
 export class CollisionDetectionDemo {
-  private canvas: Canvas;
-  private balls: BallGroup;
+  private canvas: Canvas
+  private balls: BallGroup
   constructor(parent: HTMLElement) {
-    this.canvas = new Canvas();
-    parent.append(this.canvas.dom);
-    this.balls = new BallGroup(this.canvas, 100);
+    this.canvas = new Canvas()
+    parent.append(this.canvas.dom)
+    this.balls = new BallGroup(this.canvas, 100)
   }
   private animate = () => {
-    this.canvas.clear();
-    this.balls.handleCollision();
-    this.balls.update();
-    this.balls.bound(this.canvas);
-    this.balls.draw(this.canvas);
-    requestAnimationFrame(this.animate);
-  };
+    this.canvas.clear()
+    this.balls.handleCollision()
+    this.balls.update()
+    this.balls.bound(this.canvas)
+    this.balls.draw(this.canvas)
+    requestAnimationFrame(this.animate)
+  }
   public app = () => {
-    this.animate();
-  };
+    this.animate()
+  }
 }
