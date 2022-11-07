d17f7a1c30c2f39a88790dd0d39816a135254cd4
yashweblife <yashweblife20@gmail.com>
2022-11-07 16:31:18 -0600

    tested graph

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,20 @@ import { ImageClassifierDemo } from "./projects/ImageClassifier";
 import { LinearRegressionDemo } from "./projects/LinearRegression";
 import { RayCastingDemo } from "./projects/RayCasting";
 import { GeneticAlgoDemo } from "./projects/GeneticAlgo";
+import { Graph, Vector } from "./lib";
 
-const test = new GeneticAlgoDemo(document.body)
-test.app()
+const graph = new Graph(document.body);
+for (var i = -Math.PI; i < Math.PI; i += 0.01) {
+  graph.addData(new Vector(i, Math.sin(i)));
+}
+graph.canvas.dom.addEventListener("wheel", (e: WheelEvent) => {
+  e.preventDefault();
+  if (e.deltaY > 0) {
+    graph.setScale(graph.scale + 0.1);
+  } else {
+    if (graph.scale > 1) {
+      graph.setScale(graph.scale - 0.1);
+    }
+  }
+});
+graph.animate();
