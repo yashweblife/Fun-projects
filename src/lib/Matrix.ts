0579f8d4d23bd60ee21fea627a31bc5d8a664239
yashweblife <yashweblife20@gmail.com>
2022-11-11 03:09:51 -0600

    Added code

--- src/lib/Matrix.ts
+++ src/lib/Matrix.ts
@@ -1,1 +1,8 @@
-export class Matrix{}
+import {Vector} from './Vector'
+export class Matrix{
+    public data:number[][]
+    public shape:Vector
+    constructor(){}
+    public add = ()=>{}
+    public scalar = ()=>{}
+}
