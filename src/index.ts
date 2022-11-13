2d389e7e71ff12b5784cbae1ee2bd499c2daee1a
yashweblife <yashweblife20@gmail.com>
2022-11-12 23:32:11 -0600

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,35 @@ import { ImageClassifierDemo } from "./projects/ImageClassifier";
 import { LinearRegressionDemo } from "./projects/LinearRegression";
 import { RayCastingDemo } from "./projects/RayCasting";
 import { GeneticAlgoDemo } from "./projects/GeneticAlgo";
-import { Graph, Vector } from "./lib";  
-import { Matrix } from "./lib";
+import { Graph, Vector } from "./lib";
+import { Matrix, bench } from "./lib";
 
-var x = new Matrix();
-var data = [
-    [0, 0, 1],
-    [0, 1, 0],
-    [1, 0, 0],
-];
-x.init(data)
-var y = new Matrix();
-var data = [
-    [0, 0, 1],
-    [0, 1, 0],
-    [1, 0, 0],
-];
-y.init(data)
+// var x = new Matrix();
+// var data = [
+//     [0, 0, 1],
+//     [0, 1, 0],
+//     [1, 0, 0],
+// ];
+// x.init(data)
+// var y = new Matrix();
+// var data = [
+//     [0, 0, 1],
+//     [0, 1, 0],
+//     [1, 0, 0],
+// ];
+// y.init(data)
+// for(var i=0;i<10;i++){
+//     x.add(y)
+//     x.addTest(y)
+// }
 
-x.add(y)
+function test() {
+  var sum = 0;
+  for (var i = 0; i < 1000; i++) {
+    for(var j=0;j<1000;j++){
+        sum += 1;
+    }
+  }
+  return sum;
+}
+bench(test)
