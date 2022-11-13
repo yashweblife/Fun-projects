8862a39217a9567952f25597f67696df4f4a2729
yashweblife <yashweblife20@gmail.com>
2022-11-12 23:54:46 -0600

    new test

--- src/index.ts
+++ src/index.ts
@@ -11,34 +11,21 @@ import { LinearRegressionDemo } from "./projects/LinearRegression";
 import { RayCastingDemo } from "./projects/RayCasting";
 import { GeneticAlgoDemo } from "./projects/GeneticAlgo";
 import { Graph, Vector } from "./lib";
-import { Matrix, bench } from "./lib";
+import { Matrix, bench, benchAverage } from "./lib";
 
-// var x = new Matrix();
-// var data = [
-//     [0, 0, 1],
-//     [0, 1, 0],
-//     [1, 0, 0],
-// ];
-// x.init(data)
-// var y = new Matrix();
-// var data = [
-//     [0, 0, 1],
-//     [0, 1, 0],
-//     [1, 0, 0],
-// ];
-// y.init(data)
-// for(var i=0;i<10;i++){
-//     x.add(y)
-//     x.addTest(y)
-// }
-
-function test() {
-  var sum = 0;
-  for (var i = 0; i < 1000; i++) {
-    for(var j=0;j<1000;j++){
-        sum += 1;
-    }
-  }
-  return sum;
-}
-bench(test)
+var x = new Matrix();
+var data = [
+    [0, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0],
+];
+x.init(data)
+var y = new Matrix();
+var data = [
+    [0, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0],
+];
+y.init(data)
+console.log(benchAverage(()=>x.add(y), 10000))
+console.log(benchAverage(()=>x.addTest(y), 10000))
