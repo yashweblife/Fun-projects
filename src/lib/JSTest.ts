31291e4eefc268d440d99720cc8424daa098280d
yashweblife <yashweblife20@gmail.com>
2022-11-13 00:50:14 -0600

    fixed some bugs

--- src/lib/JSTest.ts
+++ src/lib/JSTest.ts
@@ -1,29 +1,30 @@
-export function bench(test:()=>any){
-    //console.time("test")
-    var start = new Date()
-    test()
-    var end = new Date()
-    var final = end.getTime()-start.getTime()
-    //console.log(":::: ", final)
-    //console.timeEnd("test")
-    return(final)
+export function bench(test: () => any) {
+  //console.time("test")
+  var start = new Date();
+  test();
+  var end = new Date();
+  var final = end.getTime() - start.getTime();
+  //console.log(":::: ", final)
+  //console.timeEnd("test")
+  return final;
 }
-export function benchAverage(f:()=>any, rotations:number=1000){
-    var time = 0
-    for(var i=0;i<rotations;i++){
-        time += bench(f)
-    }
-    time/=rotations
-    return time
+export function benchAverage(f: () => any, rotations: number = 1000) {
+  var time = 0;
+  for (var i = 0; i < rotations; i++) {
+    time += bench(f);
+  }
+  time /= rotations;
+  return time;
 }
 
-export function test(f:()=>any){
-    var check = ()=>{
-        return(f())
-    }
-    var shouldBe = (data:any)=>{
-        if(check() == data) return(true)
-        return(false)    
-    }
-    return({shouldBe})
+export function testIf(f: () => any) {
+  var check = () => {
+    return f();
+  };
+  var shouldBe = (data: any) => {
+    if (check() == data) return true;
+    return false;
+  };
+  return { shouldBe };
 }
+
