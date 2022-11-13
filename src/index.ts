deb1a9af15a59860360f4fe00552cad66624f3f2
yashweblife <yashweblife20@gmail.com>
2022-11-13 16:27:24 -0600

    image classifier test

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,6 @@ import { LinearRegressionDemo } from "./projects";
 import { RayCastingDemo } from "./projects";
 import { GeneticAlgoDemo } from "./projects";
 import { Graph, Vector, Matrix, bench, benchAverage, testIf } from "./lib";
+
+var demo = new ImageClassifierDemo()
+demo.app()
