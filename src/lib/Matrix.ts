cb814e55a542416fc03e06e00ee382802b79226a
yashweblife <yashweblife20@gmail.com>
2022-11-12 23:55:11 -0600

    nothing new

--- src/lib/Matrix.ts
+++ src/lib/Matrix.ts
@@ -1,44 +1,38 @@
-import {Vector} from './Vector'
+import { Vector } from "./Vector";
 
-export class Matrix{
-    public data:number[][]
-    public shape:Vector
-    constructor(){}
-    public init = (a:number[][])=>{
-        this.data = a;
+export class Matrix {
+  public data: number[][];
+  public shape: Vector;
+  constructor() {}
+  public init = (a: number[][]) => {
+    this.data = a;
+  };
+  public $ = (x: number, y: number): number => {
+    return this.data[x][y];
+  };
+  public addTest = (mat: Matrix) => {
+    var output = [];
+    for (var i = 0; i < this.data.length; i++) {
+      var test = [];
+      for (var j = 0; j < this.data[i].length; j++) {
+        test.push(this.data[i][j] + mat.data[i][j]);
+      }
+      output.push(test);
     }
-    public $ = (x:number, y:number):number=>{
-        return(this.data[x][y])
+  };
+  public add = (mat: Matrix) => {
+    var rows = [];
+    for (var i = 0; i < this.data.length; i++) {
+      rows.push([this.data[i], mat.data[i]]);
     }
-    public addTest = (mat:Matrix)=>{
-        console.time('test 0')
-        var output = []
-        for(var i=0;i<this.data.length;i++){
-            var test = []
-            for(var j=0;j<this.data[i].length;j++){
-                test.push(this.data[i][j] + mat.data[i][j])
-            }
-            output.push(test)
-        }
-        //console.log(output)
-        console.timeEnd('test 0')
+    var output = [];
+    for (var i = 0; i < rows.length; i++) {
+      var test = [];
+      for (var j = 0; j < rows[i][0].length; j++) {
+        test.push(rows[i][0][j] + rows[i][1][j]);
+      }
+      output.push(test);
     }
-    public add = (mat:Matrix)=>{
-        console.time('test 1')
-        var rows = []
-        for(var i=0;i<this.data.length;i++){
-            rows.push([this.data[i],mat.data[i]])
-        }
-        var output = []
-        for(var i=0;i<rows.length;i++){
-            var test = []
-            for(var j=0;j<rows[i][0].length;j++){
-                test.push(rows[i][0][j] + rows[i][1][j])
-            }
-            output.push(test)
-        }
-        //console.log(output)
-        console.timeEnd('test 1')
-    }
-    public scalar = ()=>{}
+  };
+  public scalar = () => {};
 }
