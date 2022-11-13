1c575b5e6f3e7989e77b8ff0d6e9bfdb25d2a6c6
yashweblife <yashweblife20@gmail.com>
2022-11-13 16:40:34 -0600

    Added more functions

--- src/projects/ImageClassifier.ts
+++ src/projects/ImageClassifier.ts
@@ -8,10 +8,10 @@ export class ImageClassifierDemo {
   private videoElement: HTMLVideoElement;
   private canvas: Canvas;
   private camera: MediaStream;
-  constructor(parent:HTMLElement = document.body) {
+  constructor(parent: HTMLElement = document.body) {
     this.videoElement = document.createElement("video");
     this.canvas = new Canvas();
-    parent.append(this.canvas.dom)
+    parent.append(this.canvas.dom);
     this.canvas.setSize(300, 300);
     navigator.mediaDevices
       .getUserMedia({
@@ -29,15 +29,41 @@ export class ImageClassifierDemo {
         console.log(err.message);
       });
   }
-  private getPixelArray = ()=>{
-    const image:Uint8ClampedArray = this.canvas.getImageData()
-    var pixels:Vector[] = []
-  }
+  public reduceRes = (data: Vector[][], res: number = 4) => {
+    var output = [] as Vector[][];
+    for (var i = 0; i < data.length; i += res) {
+      let line = [] as Vector[];
+      for (var j = 0; j < data[i].length; j += res) {
+        line.push(data[i][j]);
+      }
+      output.push(line);
+    }
+    return output;
+  };
+  private getPixelArray = () => {
+    const image: ImageData = this.canvas.getImageData();
+    const data:Uint8ClampedArray = image.data
+    var pixels: Vector[] = [];
+    for (var i = 0; i < data.length; i += 4) {
+      let pixel = new Vector(data[i + 0], data[i + 1], data[i + 2]);
+      pixels.push(pixel);
+    }
+    let output:Vector[][] = []
+    for (var i = 0; i < image.height; i++) {
+      let line = [] as Vector[];
+      for (var j = 0; j < image.width; j++) {
+        line.push(pixels[i * image.width + j]);
+      }
+      output.push(line);
+    }
+
+  };
+
   private animate = () => {
-    this.canvas.drawImage(this.videoElement )
-    requestAnimationFrame(this.animate)
-  }
+    this.canvas.drawImage(this.videoElement);
+    requestAnimationFrame(this.animate);
+  };
   public app = () => {
-    this.animate()
+    this.animate();
   };
 }
