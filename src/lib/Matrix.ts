57340a62f4d7524f7b3e26a276de69f87b4b279e
yashweblife <yashweblife20@gmail.com>
2022-11-12 20:45:41 -0600

    Added more functionality

--- src/lib/Matrix.ts
+++ src/lib/Matrix.ts
@@ -1,8 +1,24 @@
 import {Vector} from './Vector'
+
+class Variable{
+    public data:Variable[]
+    constructor(val:Variable[]){
+        this.data = val
+    }
+}
 export class Matrix{
-    public data:number[][]
+    public data:Variable[]
     public shape:Vector
     constructor(){}
+    public init = (a:[])=>{
+        this.data = a;
+    }
+    public $ = (val:number[])=>{
+        for(var i=0;i<val.length;i++){
+
+        }
+
+    }
     public add = ()=>{}
     public scalar = ()=>{}
 }
