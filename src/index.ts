9fd77271f2f759e2fc85f812d07b32cfa479c563
yashweblife <yashweblife20@gmail.com>
2022-11-12 20:45:53 -0600

    added matrix test

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,13 @@ import { LinearRegressionDemo } from "./projects/LinearRegression";
 import { RayCastingDemo } from "./projects/RayCasting";
 import { GeneticAlgoDemo } from "./projects/GeneticAlgo";
 import { Graph, Vector } from "./lib";
+import { Matrix } from "./lib";
+
+var x = new Matrix();
+// x.init()
+var data = [
+  [0, 0, 1],
+  [0, 1, 0],
+  [1, 0, 0],
+];
+console.log(x.$([0, 0]));
