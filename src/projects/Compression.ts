b9b852ad7d2611cfa5b4e28b98de592979906b87
yashweblife <yashweblife20@gmail.com>
2022-11-13 22:35:58 -0600

    added default parent

--- src/projects/Compression.ts
+++ src/projects/Compression.ts
@@ -20,7 +20,7 @@ class ImageCompression{
  */
 export class CompressorDemo {
   private textArea: HTMLTextAreaElement;
-  constructor(parent: HTMLElement) {
+  constructor(parent: HTMLElement = document.body) {
     this.textArea = document.createElement("textarea");
     this.textArea.value =
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
