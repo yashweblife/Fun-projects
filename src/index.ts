a76ed5ec0ab605715243f4c5bc91eb27d899ab60
yashweblife <yashweblife20@gmail.com>
2022-11-13 22:46:40 -0600

    Tested "test"

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,8 @@ import { LinearRegressionDemo } from "./projects";
 import { RayCastingDemo } from "./projects";
 import { GeneticAlgoDemo } from "./projects";
 import { Graph, Vector, Matrix, bench, benchAverage, testIf } from "./lib";
+
+var test = (data:string)=>{
+    return("Hello "+ data)
+}
+console.log(testIf(()=>test("")).shouldBe("Hello "))
