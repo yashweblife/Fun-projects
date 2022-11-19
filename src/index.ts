cd6b4887bc705ffc9389d9f9465d26194c5bca98
yashweblife <yashweblife20@gmail.com>
2022-11-19 16:09:52 -0600

    tested new graph

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,20 @@ import { ImageClassifierDemo } from "./projects";
 import { LinearRegressionDemo } from "./projects";
 import { RayCastingDemo } from "./projects";
 import { GeneticAlgoDemo } from "./projects";
-import { Vector, Matrix, bench, benchAverage, testIf, Canvas } from "./lib";
+import {Graph, Vector, Matrix, bench, benchAverage, testIf, Canvas } from "./lib";
 import { FourierDemo } from "./projects";
 
-var app = new FourierDemo();
-
-app.plotButterFly();
-app.app();
+var test = new Graph()
+test.plot(()=>{
+  var output = []
+  for(var i=0;i<100;i++){
+    output.push(i)
+  }
+  return(output)
+})
+test.animate()
 
 var scale: HTMLInputElement = document.querySelector("#scale");
 scale.addEventListener("input", () => {
-  app.setScale(Number(scale.value));
+  test.setScale(Number(scale.value));
 });
