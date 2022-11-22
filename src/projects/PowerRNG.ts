5e6bbf3971bdac9929e120e60af739a777e2a2f6
yashweblife <yashweblife20@gmail.com>
2022-11-21 18:18:13 -0600

    made basic program

--- src/projects/PowerRNG.ts
+++ src/projects/PowerRNG.ts
@@ -1,5 +1,37 @@
+import { Canvas, Vector } from "../lib";
+
 export class PowerRNGDemo {
-  constructor(parent:HTMLElement = document.body) {}
-  private animate = () => {};
-  public app = () => {};
+  private canvas: Canvas = new Canvas();
+  private circles: Vector[] = [];
+  constructor(parent: HTMLElement = document.body) {
+    parent.append(this.canvas.dom)
+    this.canvas.setSize(255,255)
+    for (var i = 0; i < 3; i++) {
+      this.circles.push(Vector.rand(0, this.canvas.width));
+    }
+  }
+  private draw = ()=>{
+    var res = 2
+    for(var i=0;i<this.canvas.width;i+=res){
+      for(var j=0;j<this.canvas.height;j+=res){
+        var color = new Vector()
+        this.circles.forEach((c:Vector)=>{
+          color.add(c)
+        })
+        color.scalar(this.circles.length)
+        this.canvas.rect({
+          pos:new Vector(i,j),
+          size:new Vector(res,res),
+          fill:true,
+          fillColor:`rgb(${this.circles[0].dist(new Vector(i,j))},${this.circles[1].dist(new Vector(i,j))},${this.circles[2].dist(new Vector(i,j))})`
+        })
+      }
+    }
+  }
+  private animate = () => {
+    this.draw()
+  };
+  public app = () => {
+    this.animate()
+  };
 }
